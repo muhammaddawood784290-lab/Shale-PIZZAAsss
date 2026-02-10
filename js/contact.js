e2f56146b73429b0d1861dc79726d8264246fefc
muhammaddawood784290-lab <muhammaddawood784290@gmail.com>
2026-02-11 04:07:53 +0500

    Form Alert Modified

--- js/contact.js
+++ js/contact.js
@@ -1,8 +1,41 @@
-// Fake submit (project demo ke liye)
-$("#contactForm").on("submit", function (e) {
-  e.preventDefault();
-  $("#successMsg").removeClass("d-none");
-  this.reset();
+console.log("Contact page loaded ✅");
+
+
+// Name field validation
+$("#name").on("input", function () {
+  let value = $(this).val();
+
+  // sirf letters + space allow
+  let cleanValue = value.replace(/[^a-zA-Z\s]/g, "");
+
+  if (value !== cleanValue) {
+    $(this).val(cleanValue);
+  }
 });
 
-console.log("Contact page loaded ✅");
+
+// Fake submit (project demo)
+$("#contactForm").on("submit", function (e) {
+  e.preventDefault();
+
+  Swal.fire({
+    icon: "success",
+    title: "Message Sent 🍕",
+    text: "We’ll contact you shortly!",
+    showConfirmButton: false,
+    timer: 2000,
+    timerProgressBar: true,
+
+    // SweetAlert2 built-in animations
+    showClass: {
+      popup: "swal2-show"
+    },
+    hideClass: {
+      popup: "swal2-hide"
+    },
+
+    backdrop: "rgba(0,0,0,0.6)"
+  });
+
+  this.reset();
+});
