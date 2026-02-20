914add7feecac74aa3013378c66c8df6bf3d2e35
muhammaddawood784290-lab <muhammaddawood784290@gmail.com>
2026-02-21 04:07:14 +0500

    Menu page done but without some FUNCTION!!! :(

--- js/menu.js
+++ js/menu.js
@@ -209,3 +209,25 @@ gsap.from(".special-hero .hero-subtitle", {
 $(document).ready(function() {
   menuAnimation();
 });
+
+barba.init({
+  transitions: [
+    {
+      name: 'fade',
+      leave(data) {
+        return gsap.to(data.current.container, {
+          opacity: 0,
+          duration: 0.5
+        });
+      },
+      enter(data) {
+        return gsap.from(data.next.container, {
+          opacity: 0,
+          duration: 0.5
+        });
+      }
+    }
+  ]
+});
+
+
